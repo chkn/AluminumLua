40373852a3fcfc6344b11a1566ca6b3c6ee60a4f
Game Master <gamemaster@101gr.com>
2012-12-17 23:23:07 +0400

    NuGet packages

---
+++ vs2010/AluminumLua.Tests/Tests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace AluminumLua.Tests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void DoFileTest()
+		{
+			LuaParser parser;
+
+			var context = new LuaContext();
+			context.AddBasicLibrary();
+			context.AddIoLibrary();
+
+			parser = new LuaParser(context, "dofile.lua");
+			parser.Parse();
+		}
+
+		[Test]
+		public void FunctionScopesTest()
+		{
+			LuaParser parser;
+
+			var context = new LuaContext();
+			context.AddBasicLibrary();
+			context.AddIoLibrary();
+
+			parser = new LuaParser(context, "functionscopes.lua");
+			parser.Parse();
+		}
+
+		[Test]
+		public void HelloWorldTest()
+		{
+			LuaParser parser;
+
+			var context = new LuaContext();
+			context.AddBasicLibrary();
+			context.AddIoLibrary();
+
+			parser = new LuaParser(context, "helloworld.lua");
+			parser.Parse();
+		}
+	}
+}
